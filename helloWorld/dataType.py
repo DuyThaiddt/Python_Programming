89b60200a8fc5f10eb408789b9bd007e3a12e593
Đào Duy Thái <daoduythai.working@gmail.com>
2022-08-08 00:44:21 +0700

    inputOutput added

---
+++ helloWorld/dataType.py
@@ -0,0 +1,67 @@
+# name = "bro"
+# # print("hello "+name)
+# # print(type(name))
+#
+# first_name = "Bro"
+# last_name = "Code"
+# full_name = first_name +" "+ last_name
+# print("hello " + full_name)
+
+# age = 20
+# age += 1
+# print(age)
+# print(type(age))
+# print("My age is: "+ str(age))
+
+# height= 177.0
+# print(height)
+# print(type(height))
+# print("My height is: "+ str(height)  + "cm)")
+
+# human = True
+# print(human)
+# print(type(human))
+# print("Are you a human "+str(human))
+
+
+# name = "Duy Thai"
+# age = 19
+# attractive = True
+
+# name, age, attractive = "Duy Thai", 21, True
+#
+# print(name)
+# print(age)
+# print(attractive)
+
+
+# Spongebob = Patrick = Sandy = Squidward = 15
+
+# name = "Duy Thai"
+# print(len(name))
+# print(name.find("T"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("a"))
+# print(name.replace("u", "a0"))
+# print(name*3)
+
+#type casting = converting date type to another data type
+x = 1   #int
+y = 2.0 #float
+z = "3" #str
+
+y = int(y)
+
+print("X is: " + str(x)*10)
+print(y)
+print(float(z)*3)
+
+
+
+
+
+
