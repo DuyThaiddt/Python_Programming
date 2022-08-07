d1957dd29d0d1bc7ba1a441375b52bb0ba9b0761
Đào Duy Thái <daoduythai.working@gmail.com>
2022-08-08 00:01:50 +0700

    adding main file

---
+++ helloWorld/main.py
@@ -0,0 +1,13 @@
+# name = "bro"
+# # print("hello "+name)
+# # print(type(name))
+#
+# first_name = "Bro"
+# last_name = "Code"
+# full_name = first_name +" "+ last_name
+# print("hello " + full_name)
+
+age = 20
+age += 1
+print(age)
+print(type(age))
