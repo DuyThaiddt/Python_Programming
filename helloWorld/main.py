83f6c8f4f0645702d911200e588341ef164ce060
Đào Duy Thái <daoduythai.working@gmail.com>
2022-08-08 00:17:01 +0700

    new

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -7,7 +7,21 @@
 # full_name = first_name +" "+ last_name
 # print("hello " + full_name)
 
-age = 20
-age += 1
-print(age)
-print(type(age))
+# age = 20
+# age += 1
+# print(age)
+# print(type(age))
+# print("My age is: "+ str(age))
+
+# height= 177.0
+# print(height)
+# print(type(height))
+# print("My height is: "+ str(height)  + "cm)")
+
+human = True
+print(human)
+print(type(human))
+print("Are you a human "+str(human))
+
+
+
